a79a4706f579a45bf42a0647e1f70db2f0fdadad
ConnorHoare <connor@tipcigroup.com>
2022-11-07 15:14:57 +0000

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
 // api url - returns 12 users in json format inlcuding name email picture and location
 const employeeUrl = "https://randomuser.me/api/?format=json&inc=picture,name,email,location&results=12";
-
-// put url into json using AJAX
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data)
-    }
-  }
-  xhr.send();
-}
+let employees = null;
+// get gallety container
+let galleryContainer = document.getElementById('gallery');
 
 function createSearchElement() {
   // get the search container div
@@ -29,9 +19,8 @@ function createSearchElement() {
   return searchContainer
 }
 
-function createGallery(data) {
-  // get gallety container
-  let galleryContainer = document.getElementById('gallery');
+function createGallery(data, index) {
+  
 
   // create employee card using data passed in by api call
   let employeeCard = `<div class="card">
@@ -47,37 +36,101 @@ function createGallery(data) {
 
   // add employeeCard to the gallery container
    galleryContainer.insertAdjacentHTML("beforeend", employeeCard);
+   galleryContainer.lastElementChild.addEventListener("click", (event) => {
+    createModal(employees[index], false)
+   })
    // return the container
    return galleryContainer
 }
 
 // create pop up modal
-function createModal() {
-  let modal = `<div class="modal-container">
+function createModal(data, isError) {
+  if (!isError) {
+    // create dob string based on the result from dob in api call
+    const dob = data.dob.date;
+    const date = dob.substr(5,2) + "/" + dob.substr(8,2) + "/" + dob.substr(0,4);
+    // create modal
+    let modal = `<div class="modal-container">
                   <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                   <div class="modal-info-container">
-                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">name</h3>
-                    <p class="modal-text">email</p>
-                    <p class="modal-text cap">city</p>
+                    <img class="modal-img" src="${data.picture.medium}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                    <p class="modal-text">${data.email}</p>
+                    <p class="modal-text cap">${data.location.city}</p>
                     <hr>
-                    <p class="modal-text">(555) 555-5555</p>
-                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                    <p class="modal-text">Birthday: 10/21/2015</p>
+                    <p class="modal-text">${data.phone}</p>
+                    <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${date}</p>
                   </div>
                   </div>
                 </div>`
-  document.body.insertAdjacentHTML("beforeend", modal);
+
+    // insert modal to the gallery container
+    galleryContainer.insertAdjacentHTML("beforeend", modal);
+
+    // remove modal by closing modal window
+    let closeBtn = document.getElementById('modal-close-btn');
+    closeBtn.addEventListener("click", () => {
+      let modalContainer = galleryContainer.querySelector(".modal-container");
+      galleryContainer.removeChild(modalContainer);
+    });
+
+  } else {
+    let errorModal = `
+      <div class="modal-container">
+        <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${data}</h3>
+        </div>
+      </div>
+    `
+
+    galleryContainer.insertAdjacentHTML("beforeend", errorModal);
+
+    let closeBtn = document.getElementById('modal-close-btn');
+    closeBtn.addEventListener('click', () => {
+      let modalContainer = galleryContainer.querySelector(".modal-container");
+      galleryContainer.removeChild(modalContainer);
+    });
+  }
+  
+  
 }
 
+// create async func as results might not load straight away
+async function run() {
+  // create empty fetch response 
+  let fetchResponse = null;
 
-// get the employee array and map over to get each object and pass into createGallery function
-getJSON(employeeUrl, (json) => {
-  json.results.map (person => {
-    createGallery(person);
-  })
-})
-createSearchElement();
-// createGallery();
-// loadEmployees();
+  // try fetch the emplyeeUrl Api
+  try {
+    fetchResponse = await fetch(employeeUrl);
+    // otherwise catch the error and create the modal with the default presets for errors
+  } catch (error) {
+    createModal(error, true);
+  }
+
+  // if there is no error and the fetch request is ok
+  if (fetchResponse != null && fetchResponse.ok) {
+    // add the json from the fetch response to json var
+    let json = await fetchResponse.json();
+    // add the results from the api to the employees
+    employees = json.results
+
+    if (employees != null) {
+      galleryContainer.innerHTML = ''
+      for (var i = 0; i < employees.length; i++) {
+        const employee = employees[i];
+        createGallery(employee, i)
+      }
+    } else {
+      createModal("Could not fetch information", true)
+    }
+  }
+}
+
+run();
+
